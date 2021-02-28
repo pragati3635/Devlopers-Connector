579b1689536c5fafb1f99de335596d54f1d45069
Pragati Sinha <pragati3635@gmail.com>
2021-02-28 17:17:53 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express= require('express');
+const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const auth = require('./routes/api/auth');
 const app= express();
-app.get()
+
+connectDB();
+
+app.use(express.json({extended : false}));
+
+app.get('/',(req,res) => res.send('API Running'));
+
+// define routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+app.use('/api/auth',auth);
+
 const PORT = 5000 || process.env.PORT;
+
 app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
