f8f4bd9084d107cbe45c3d341de9c8786c406c90
Pragati Sinha <pragati3635@gmail.com>
2021-03-31 00:56:14 +0530

    frontend

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React , {Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          < Route exact path = "/" component = {Landing} />
+          <section className = "container" > 
+            <Switch>
+              <Route exact path = "/register" component = { Register } />
+              <Route exact path = "/login" component = { Login } />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
