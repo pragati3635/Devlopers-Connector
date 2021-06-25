936410b441111ad8947682298085aff234e1ecf7
pragati3635 <59009348+pragati3635@users.noreply.github.com>
2021-06-25 14:04:13 +0530

    Update types.js

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,6 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REG_SUCCESS = 'REG_SUCCESS';
+export const REG_FAILURE = 'REG_FAILURE';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
