65824b4c6f71cbfe201f6e57e82bb87eeb3ed0a7
pragati3635 <59009348+pragati3635@users.noreply.github.com>
2021-06-25 14:31:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React , {Fragment} from 'react';
+import React , {Fragment , useEffect} from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import { BrowserRouter as Router , Route , Switch} from 'react-router-dom';
 //redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 
 import './App.css';
 
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  } , []);
+
   return (
     <Provider store={store}>
       <Router>
@@ -18,6 +30,7 @@ const App = () => {
           <Navbar />
           < Route exact path = "/" component = {Landing} />
           <section className = "container" > 
+            <Alert/>
             <Switch>
               <Route exact path = "/register" component = { Register } />
               <Route exact path = "/login" component = { Login } />
